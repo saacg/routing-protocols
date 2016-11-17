61ba1ef110f5b945262c33ca4930c7381a2fb55c
Luke Osborne <losborne@bu.edu>
2016-11-17 11:27:16 -0500

    Get it together!
    Edited with VIM.

--- Event.java
+++ Event.java
@@ -1,82 +1,66 @@
-public class Event
-{
+public class Event {
     private double time;
     private int type;
     private int entity;
     private Packet packet;
     
-    public Event(double t, int ty, int ent)
-    {
+    public Event(double t, int ty, int ent) {
         time = t;
         type = ty;
         entity = ent;
         packet = null;
     }
     
-    public Event(double t, int ty, int ent, Packet p)
-    {
+    public Event(double t, int ty, int ent, Packet p) {
         time = t;
         type = ty;
         entity = ent;
         packet = new Packet(p);
     }    
             
-    public boolean setTime(double t)
-    {
+    public boolean setTime(double t) {
         time = t;
         return true;
     }
     
-    public boolean setType(int n)
-    {
+    public boolean setType(int n) {
         type = n;
         return true;
     }
     
-    public boolean setEntity(int n)
-    {
+    public boolean setEntity(int n) {
         entity = n;
         return true;
     }
     
-    public boolean setPacket(Packet p)
-    {
-        if (p == null)
-        {
+    public boolean setPacket(Packet p) {
+        if (p == null) {
             packet = null;
-        }        
-        else
-        {
+        } else {
             packet = new Packet(p.sourceid, p.destid, p.mincost);
         }
         
         return true;
     }
     
-    public double getTime()
-    {
+    public double getTime() {
         return time;
     }
     
-    public int getType()
-    {
+    public int getType() {
         return type;
     }
     
-    public int getEntity()
-    {
+    public int getEntity() {
         return entity;
     }
     
-    public Packet getPacket()
-    {
+    public Packet getPacket() {
         return packet;
     }
     
-    public String toString()
-    {
+    public String toString() {
         return("time: " + time + "  type: " + type + "  entity: " + entity +
                "packet: " + packet);
-    }
-        
+    }    
 }
